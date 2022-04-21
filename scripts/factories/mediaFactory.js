a7603eaf03a5ec7416597b1f27ae2643d7e8af78
Soundripper <jerrsoundripper@gmail.com>
2022-04-21 12:01:25 +0200

    Controls an muted video thumb ios

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -22,14 +22,12 @@ function mediaFactory(media){
     }else if(media.hasOwnProperty("video")){
         const imageDiv = document.createElement('video');
         imageDiv.classList.add("imageThumb");
-        imageDiv.setAttribute("preload", "metadata");
-        const videoDiv = document.createElement('source');
-        videoDiv.setAttribute("src", `assets/photosTotal/${video}#t=0.001`);
-        videoDiv.setAttribute("type", "video/mp4");
+        imageDiv.setAttribute("src", `assets/photosTotal/${video}#t=0.001`);
+        imageDiv.setAttribute("controls", "true");
+        imageDiv.setAttribute("muted", "true");
         imageCardButton.appendChild(imageDiv);
-        imageDiv.appendChild(videoDiv);
-        videoDiv.setAttribute("aria-label", title);
-        videoDiv.setAttribute("aria-describedby", "versGalerie");
+        imageDiv.setAttribute("aria-label", title);
+        imageDiv.setAttribute("aria-describedby", "versGalerie");
     }
     
     const imageInfos = document.createElement('div');
